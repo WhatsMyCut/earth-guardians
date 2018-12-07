15a053bb33b2d03d6edde0d87a90029815ae2f98
Edward Manda <lilkups@yahoo.com>
2018-12-06 18:44:14 -0600

    prototyping the phone up down, wip - proper styling

--- mobile/components/LandingPage.js
+++ mobile/components/LandingPage.js
@@ -2,10 +2,9 @@ import React from 'react';
 import { StoreConsumer } from '../store/Store';
 import Home from './Home';
 import PhoneSignup from './PhoneSignup';
-import Test from './Test';
 
 export default class LandingPage extends React.Component {
   render() {
-    return <StoreConsumer>{value => <Test {...value} />}</StoreConsumer>;
+    return <StoreConsumer>{value => <PhoneSignup {...value} />}</StoreConsumer>;
   }
 }
--- mobile/components/PhoneInputComp.js
+++ mobile/components/PhoneInputComp.js
@@ -1,8 +1,49 @@
 import React from 'react';
-import { StyleSheet, TextInput } from 'react-native';
+import { StyleSheet, View, TextInput } from 'react-native';
+import { Dropdown } from 'react-native-material-dropdown';
 
 export default class PhoneInputComp extends React.Component {
+  state = {
+    valid: false,
+    selectedCountry: 'US',
+    countryCode: '1',
+    number: '',
+    counries: [{ code: 1, name: 'US' }, { code: 260, name: 'ZM' }],
+  };
+
+  updateInfo = () => {};
   render() {
-    return <TextInput placeholder={'Phone number'} />;
+    return (
+      <View style={styles.container}>
+        <Dropdown
+          data={this.state.counries}
+          valueExtractor={({ name }) => name}
+          labelExtractor={({ label }) => label}
+          containerStyle={styles.countryCode}
+          value={this.state.counries[0].name + ' +1'}
+          rippleCentered={true}
+        />
+        <TextInput
+          style={styles.number}
+          placeholder={'Phone number'}
+          onChangeText={number => this.setState({ number })}
+          value={this.state.number}
+        />
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    borderColor: 'red',
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  countryCode: {
+    width: 80,
+  },
+  number: { color: 'red', height: 30, borderColor: 'red', borderWidth: 1 },
+});
