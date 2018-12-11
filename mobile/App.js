a90925d3534687110d62936526c55ca86395c526
Daniel Ashcraft <ashcraftbookkeeping@gmail.com>
2018-12-10 18:03:39 -0600

    Pushing the new navigation and navigation service, with referenced

--- mobile/App.js
+++ mobile/App.js
@@ -5,6 +5,10 @@ import { StoreProvider } from './store/Store';
 import AppNavigator from './navigation/AppNavigator';
 import NavigationService from './navigation/navigationService';
 
+
+// For info on how to use navigation service
+// https://reactnavigation.org/docs/en/navigating-without-navigation-prop.html#docsNav
+
 export default class App extends React.Component {
   // feed the store to the app
   render() {
