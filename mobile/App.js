ec73ce6352728f1165d2459e090d609b7d2da8ba
Daniel Ashcraft <ashcraftbookkeeping@gmail.com>
2018-12-10 18:02:57 -0600

    Pushing the new navigation and navigation service, with referenced

--- mobile/App.js
+++ mobile/App.js
@@ -3,13 +3,17 @@ import React from 'react';
 import { StoreProvider } from './store/Store';
 
 import AppNavigator from './navigation/AppNavigator';
+import NavigationService from './navigation/navigationService';
 
 export default class App extends React.Component {
   // feed the store to the app
   render() {
+    console.log('this is working');
     return (
       <StoreProvider>
-        <AppNavigator />
+        <AppNavigator ref={navigatorRef => {
+          NavigationService.setTopLevelNavigator(navigatorRef);
+        }} />
       </StoreProvider>
     );
   }
