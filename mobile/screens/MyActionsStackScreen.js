daa6789b0dd9b5ec88858dbd171399ec4c718fcf
Edward Manda <lilkups@yahoo.com>
2018-12-11 17:50:24 -0600

    my actions is populating a two column grid with an image and text overlay

--- mobile/screens/MyActionsStackScreen.js
+++ mobile/screens/MyActionsStackScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   SafeAreaView,
+  ImageBackground,
   Image,
   Platform,
   FlatList,
@@ -17,83 +18,48 @@ import {
   CardButton,
   CardImage,
 } from 'react-native-material-cards';
+
+import { actions } from './dummyData.json';
 export default class MyActionsStackScreen extends React.Component {
   static navigationOptions = {
     header: null,
   };
+  state = {
+    photos: [],
+  };
 
   render() {
+    console.log(actions);
     return (
       <SafeAreaView style={{ flex: 1 }}>
         <View style={styles.container}>
           <FlatList
-            data={[
-              { key: 'Devin1' },
-              { key: 'Jackson11' },
-              { key: 'James111' },
-              { key: 'Joel1111' },
-              { key: 'John111111' },
-              { key: 'Jillian1111' },
-              { key: 'Jimmy111111' },
-              { key: 'Julie1111' },
-              { key: 'Devin2' },
-              { key: 'Jackson2' },
-              { key: 'James2' },
-              { key: 'Joel2' },
-              { key: 'John2' },
-              { key: 'Jillian2' },
-              { key: 'Jimmy2' },
-              { key: 'Julie2' },
-              { key: 'Devin3' },
-              { key: 'Jackson3' },
-              { key: 'James3' },
-              { key: 'Joel3' },
-              { key: 'John3' },
-              { key: 'Jillian3' },
-              { key: 'Jimmy3' },
-              { key: 'Julie3' },
-              { key: 'Devin4' },
-              { key: 'Jackson4' },
-              { key: 'James4' },
-              { key: 'Joel4' },
-              { key: 'John4' },
-              { key: 'Jillian4' },
-              { key: 'Jimmy4' },
-              { key: 'Julie4' },
-              { key: 'Devin5' },
-              { key: 'Jackson5' },
-              { key: 'James5' },
-              { key: 'Joel5' },
-              { key: 'John5' },
-              { key: 'Jillian5' },
-              { key: 'Jimmy5' },
-              { key: 'Julie5' },
-              { key: 'Devin' },
-              { key: 'Jackson' },
-              { key: 'James' },
-              { key: 'Joel' },
-              { key: 'John' },
-              { key: 'Jillian' },
-              { key: 'Jimmy' },
-              { key: 'Julie' },
-            ]}
-            renderItem={({ item }) => (
-              <Card>
-                <CardImage
-                  source={{ uri: 'http://placehold.it/480x270' }}
-                  title="Above all i am here"
+            numColumns={2}
+            data={actions}
+            renderItem={({ item, index }) => (
+              <View style={styles.item}>
+                <Image
+                  source={{ uri: 'https://picsum.photos/200/300/?random' }}
+                  style={{
+                    flex: 1,
+                    width: null,
+                    height: null,
+                    borderRadius: 10,
+                  }}
                 />
-                <CardTitle
-                  title="This is a title"
-                  subtitle="This is subtitle"
-                />
-
-                <CardContent style={styles.item} text={item.key} />
-                <CardAction separator={true} inColumn={false}>
-                  <CardButton onPress={() => {}} title="Push" color="blue" />
-                  <CardButton onPress={() => {}} title="Later" color="blue" />
-                </CardAction>
-              </Card>
+                <Text
+                  style={{
+                    position: 'absolute',
+                    bottom: 10,
+                    left: 10,
+                    fontWeight: 'bold',
+                    color: '#fff',
+                    fontSize: 18,
+                  }}
+                >
+                  {item.action.substring(0, 50)}
+                </Text>
+              </View>
             )}
           />
         </View>
@@ -108,10 +74,18 @@ const styles = StyleSheet.create({
     paddingTop: 10,
   },
   item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
+    flex: 1,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 1,
+    margin: 5,
+    marginTop: 10,
+    minHeight: 200,
+    maxHeight: 230,
+    borderColor: 'gray',
     borderWidth: 1,
-    ///borderColor: 'red',
+    borderRadius: 10,
   },
 });
