e50d7847f06f5e9d766406ebaec6bdd0de84c316
Edward Manda <lilkups@yahoo.com>
2018-12-27 14:47:43 -0600

    impact information placed into the profile page and styled

--- mobile/components/shared/profile/ImpactComponent.js
+++ mobile/components/shared/profile/ImpactComponent.js
@@ -7,17 +7,95 @@ export default class ImpactComponent extends React.Component {
       <View
         style={{
           backgroundColor: '#666',
-          justifyContent: 'center',
+          justifyContent: 'space-around',
           alignItems: 'center',
           width: SCREEN_WIDTH * 0.9,
           borderRadius: 20,
           height: 120,
           marginVertical: 5,
+          flexDirection: 'row',
         }}
       >
-        <Text style={{ color: '#fff', fontSize: 18 }}>
-          Your Impact | Community
-        </Text>
+        <View>
+          <Text>{null}</Text>
+          <Text style={{ color: '#fff', marginBottom: 10, fontSize: 11 }}>
+            CO2 (lbs)
+          </Text>
+          <Text style={{ color: '#fff', marginBottom: 10, fontSize: 11 }}>
+            H2O (lbs)
+          </Text>
+          <Text style={{ color: '#fff', fontSize: 11 }}>Waste (lbs)</Text>
+        </View>
+        <View>
+          <Text style={{ color: '#fff', marginBottom: 10, fontSize: 10 }}>
+            Your Impact
+          </Text>
+          <Text
+            style={{
+              color: '#fff',
+              marginBottom: 5,
+              fontSize: 18,
+              fontWeight: 'bold',
+            }}
+          >
+            125
+          </Text>
+          <Text
+            style={{
+              color: '#fff',
+              marginBottom: 5,
+              fontSize: 18,
+              fontWeight: 'bold',
+            }}
+          >
+            471
+          </Text>
+          <Text
+            style={{
+              color: '#fff',
+              marginBottom: 5,
+              fontSize: 18,
+              fontWeight: 'bold',
+            }}
+          >
+            801
+          </Text>
+        </View>
+        <View>
+          <Text style={{ color: '#fff', marginBottom: 10, fontSize: 10 }}>
+            Community
+          </Text>
+          <Text
+            style={{
+              color: '#fff',
+              marginBottom: 5,
+              fontSize: 18,
+              fontWeight: 'bold',
+            }}
+          >
+            27,894
+          </Text>
+          <Text
+            style={{
+              color: '#fff',
+              marginBottom: 5,
+              fontSize: 18,
+              fontWeight: 'bold',
+            }}
+          >
+            103,720
+          </Text>
+          <Text
+            style={{
+              color: '#fff',
+              marginBottom: 5,
+              fontSize: 18,
+              fontWeight: 'bold',
+            }}
+          >
+            27,940
+          </Text>
+        </View>
       </View>
     );
   }
