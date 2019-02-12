6c3eeaf74111a45b7b0712f57df28ee41021ba79
Edward Manda <lilkups@yahoo.com>
2019-02-12 13:05:58 -0600

    added the device_id to the mutations and the password modal

--- mobile/components/graphql/mutations/signup_mutation.js
+++ mobile/components/graphql/mutations/signup_mutation.js
@@ -1,16 +1,14 @@
 import gql from 'graphql-tag';
 
-
 export const SIGNUP = gql`
- mutation($username: String!, $password: String!){
-  signup(username:$username, password:$password){
-      token
-      user{
-        id
-        username
-        phone
-      }
-  }
-}
-
-`
+	mutation($username: String!, $password: String!, $device_id: String!) {
+		signup(username: $username, password: $password, device_id: $device_id) {
+			token
+			user {
+				id
+				username
+				phone
+			}
+		}
+	}
+`;
--- mobile/store/Store.js
+++ mobile/store/Store.js
@@ -10,125 +10,121 @@ const Store = React.createContext();
 // create a provide
 //create a provider
 export class StoreProvider extends React.Component {
-  state = {
-    isLoading: true,
-    authenticated: false,
-    user: { number: '', country_dial_code: '' },
-  };
+	state = {
+		isLoading: true,
+		authenticated: false,
+		user: { number: '', country_dial_code: '' },
+	};
 
-  async componentDidMount() {
-    try {
+	async componentDidMount() {
+		try {
+			// check if async storage has the data
+			const phone = await RetrieveData('phone');
+			const country_dial_code = await RetrieveData('country_dial_code');
+			const store_data = await RetrieveData('EARTH_GUARDIANS_TOKEN');
+			// await StoreData('phone', null);
+			// await StoreData('country_dial_code', null);
+			// await StoreData('EARTH_GUARDIANS_TOKEN', null);
+			if (phone && country_dial_code && store_data) {
+				const user = { phone, country_dial_code };
+				this.setState({ authenticated: true, user });
+				navigationService.navigate('CommunityStack', {});
+			} else {
+				await StoreData('phone', null);
+				await StoreData('country_dial_code', null);
+				await StoreData('EARTH_GUARDIANS_TOKEN', null);
+				navigationService.navigate('AuthLoading', {});
+			}
+		} catch (e) {
+			console.log(e);
+		}
+	}
+	appReady = () => {
+		this.setState({ isLoading: false });
+	};
 
-      // check if async storage has the data
-      const phone = await RetrieveData('phone');
-      const country_dial_code = await RetrieveData('country_dial_code');
-      const store_data = await RetrieveData('EARTH_GUARDIANS_TOKEN');
-      // await StoreData('phone', null);
-      // await StoreData('country_dial_code', null);
-      // await StoreData('EARTH_GUARDIANS_TOKEN', null);
-      if (phone && country_dial_code && store_data) {
-   
-        const user = { phone, country_dial_code };
-        this.setState({ authenticated: true, user });
-        navigationService.navigate('CommunityStack', {});
-      } else {
-        await StoreData('phone', null);
-        await StoreData('country_dial_code', null);
-        await StoreData('EARTH_GUARDIANS_TOKEN', null);
-        navigationService.navigate('AuthLoading', {});
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  appReady = () => {
-    this.setState({ isLoading: false });
-  };
+	authenticate = async (details) => {
+		const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+		let finalStatus = existingStatus;
+		const new_user = {
+			phone: details.phone,
+			country_dial_code: details.dialCode,
+			token: details.token,
+		};
 
-  authenticate = async details => {
-    const { status: existingStatus } = await Permissions.getAsync(
-      Permissions.NOTIFICATIONS
-    );
-    let finalStatus = existingStatus;
-    const new_user = {
-      phone: details.phone,
-      country_dial_code: details.dialCode,
-      token: details.token,
-    };
+		try {
+			// store to local storage
+			await StoreData('phone', details.phone);
+			await StoreData('country_dial_code', details.dialCode);
 
-    try {
-      // store to local storage
-      await StoreData('phone', details.phone);
-      await StoreData('country_dial_code', details.dialCode);
+			// analytics
+			const phone = details.phone;
+			const analytics = new Analytics('UA-131896215-1');
+			analytics
+				.event(new Event('Signup', 'Press', phone))
+				.then(() => console.log('success '))
+				.catch((e) => console.log(e.message));
+		} catch (e) {
+			console.log(e);
+		}
+		// for now, turn autheticate to true
+		if (finalStatus !== 'granted') {
+		}
+		// registerForPushNotificationsAsync
 
-      // analytics
-      const phone = details.phone;
-      const analytics = new Analytics('UA-131896215-1');
-      analytics
-        .event(new Event('Signup', 'Press', phone))
-        .then(() => console.log('success '))
-        .catch(e => console.log(e.message));
-    } catch (e) {
-      console.log(e);
-    }
-    // for now, turn autheticate to true
-    if (finalStatus !== 'granted') {
-    }
-    // registerForPushNotificationsAsync
+		StoreData('EARTH_GUARDIANS_TOKEN', details.token);
 
-    StoreData('EARTH_GUARDIANS_TOKEN', details.token);
+		this.setState({ authenticated: true, user: new_user });
+		navigationService.navigate('CommunityStack', {});
+	};
 
-    this.setState({ authenticated: true, user: new_user });
-    navigationService.navigate('CommunityStack', {});
-  };
+	logout = async () => {
+		try {
+			const phone = await RetrieveData('phone');
+			const analytics = new Analytics('UA-131896215-1');
+			analytics
+				.event(new Event('Logout', 'Press', phone))
+				.then(() => console.log('success '))
+				.catch((e) => console.log(e.message));
 
-  logout = async () => {
-    try {
-      const phone = await RetrieveData('phone');
-      const analytics = new Analytics('UA-131896215-1');
-      analytics
-        .event(new Event('Logout', 'Press', phone))
-        .then(() => console.log('success '))
-        .catch(e => console.log(e.message));
+			await StoreData('phone', null);
+			await StoreData('country_dial_code', null);
+			await StoreData('EARTH_GUARDIANS_TOKEN', null);
+		} catch (e) {
+			console.log(e);
+		}
+	};
 
-      await StoreData('phone', null);
-      await StoreData('country_dial_code', null);
-      await StoreData('EARTH_GUARDIANS_TOKEN', null);
-    } catch (e) {
-      console.log(e);
-    }
-  };
+	updatePhone = (phone) => {
+		// TODO: check if there is proper incoming info
+		if (phone.number && phone.country_dial_code) {
+			// make a copy of the current user
+			const update_user = { ...this.state.user };
 
-  updatePhone = phone => {
-    // TODO: check if there is proper incoming info
-    if (phone.number && phone.country_dial_code) {
-      // make a copy of the current user
-      const update_user = { ...this.state.user };
+			// update user object
+			update_user.number = phone.number;
+			update_user.country_dial_code = phone.country_dial_code;
 
-      // update user object
-      update_user.number = phone.number;
-      update_user.country_dial_code = phone.country_dial_code;
+			// update state
+			this.setState({ user: update_user });
+		}
+	};
 
-      // update state
-      this.setState({ user: update_user });
-    }
-  };
-
-  render() {
-    return (
-      <Store.Provider
-        value={{
-          store: this.state,
-          appReady: this.appReady,
-          authenticate: this.authenticate,
-          updatePhone: this.updatePhone,
-          logout: this.logout,
-        }}
-      >
-        {this.props.children}
-      </Store.Provider>
-    );
-  }
+	render() {
+		return (
+			<Store.Provider
+				value={{
+					store: this.state,
+					appReady: this.appReady,
+					authenticate: this.authenticate,
+					updatePhone: this.updatePhone,
+					logout: this.logout,
+				}}
+			>
+				{this.props.children}
+			</Store.Provider>
+		);
+	}
 }
 
 // for components to access the store
