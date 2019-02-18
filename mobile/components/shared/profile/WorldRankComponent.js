0ccf14ce807c00ca378f9344ceffbcf0fb5943d2
Mike Taylor <mike@whatsmycut.com>
2019-02-18 15:20:09 -0800

    Remove linear gradient to match XD

--- mobile/components/shared/profile/WorldRankComponent.js
+++ mobile/components/shared/profile/WorldRankComponent.js
@@ -14,67 +14,63 @@ import ViewShot from 'react-native-view-shot';
 export default class WorldRankComponent extends React.Component {
   render() {
     return (
-      <LinearGradient
-        {...LinearGradientProps.profileItem}
-        style={ styles.linearGradientBox} >
-        <View style={styles.rankContainer}>
-          <View style={[styles.rankRow]}>
-            <Text style={styles.rankNumber}>1</Text>
-            <View style={styles.rankDetail}>
-              <Text style={styles.rankName}>
-                Iceland
-              </Text>
-              <Text style={styles.smallWhiteText}>
-                9,876,653
-              </Text>
-            </View>
-          </View>
-          <View style={styles.rankRow}>
-            <Text style={styles.rankNumber}>2</Text>
-            <View style={styles.rankDetail}>
-              <Text style={styles.rankName}>
-                Switzerland
-              </Text>
-              <Text style={styles.smallWhiteText}>
-                9,876,653
-              </Text>
-            </View>
-          </View>
-          <View style={styles.rankRow}>
-            <Text style={styles.rankNumber}>3</Text>
-            <View style={styles.rankDetail}>
-              <Text style={ styles.rankName}>
-                Costa Rica
-              </Text>
-              <Text style={styles.smallWhiteText}>
-                9,876,653
-              </Text>
-            </View>
-          </View>
-          <View style={styles.rankRow}>
-            <Text style={styles.rankNumber}>4</Text>
-            <View style={styles.rankDetail}>
-              <Text style={styles.rankName}>
-                Sweden
-              </Text>
-              <Text style={styles.smallWhiteText}>
-                9,876,653
-              </Text>
-            </View>
-          </View>
-          <View style={styles.rankRow}>
-            <Text style={styles.rankNumber}>5</Text>
-            <View style={styles.rankDetail }>
-              <Text style={styles.rankName}>
-                Norway
-              </Text>
-              <Text style={styles.smallWhiteText}>
-                9,876,653
-              </Text>
-            </View>
+      <View style={styles.rankContainer}>
+        <View style={[styles.rankRow]}>
+          <Text style={styles.rankNumber}>1</Text>
+          <View style={styles.rankDetail}>
+            <Text style={styles.rankName}>
+              Iceland
+            </Text>
+            <Text style={styles.smallWhiteText}>
+              9,876,653
+            </Text>
           </View>
         </View>
-      </LinearGradient>
+        <View style={styles.rankRow}>
+          <Text style={styles.rankNumber}>2</Text>
+          <View style={styles.rankDetail}>
+            <Text style={styles.rankName}>
+              Switzerland
+            </Text>
+            <Text style={styles.smallWhiteText}>
+              9,876,653
+            </Text>
+          </View>
+        </View>
+        <View style={styles.rankRow}>
+          <Text style={styles.rankNumber}>3</Text>
+          <View style={styles.rankDetail}>
+            <Text style={ styles.rankName}>
+              Costa Rica
+            </Text>
+            <Text style={styles.smallWhiteText}>
+              9,876,653
+            </Text>
+          </View>
+        </View>
+        <View style={styles.rankRow}>
+          <Text style={styles.rankNumber}>4</Text>
+          <View style={styles.rankDetail}>
+            <Text style={styles.rankName}>
+              Sweden
+            </Text>
+            <Text style={styles.smallWhiteText}>
+              9,876,653
+            </Text>
+          </View>
+        </View>
+        <View style={styles.rankRow}>
+          <Text style={styles.rankNumber}>5</Text>
+          <View style={styles.rankDetail }>
+            <Text style={styles.rankName}>
+              Norway
+            </Text>
+            <Text style={styles.smallWhiteText}>
+              9,876,653
+            </Text>
+          </View>
+        </View>
+      </View>
     );
   }
 }
