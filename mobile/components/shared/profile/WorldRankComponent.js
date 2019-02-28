7bb6c141fecbc39b2d0b5da07630d232db816ba7
Mike Taylor <mike@whatsmycut.com>
2019-02-27 17:33:10 -0800

    World Rankings now working from GraphQL

--- mobile/components/shared/profile/WorldRankComponent.js
+++ mobile/components/shared/profile/WorldRankComponent.js
@@ -11,71 +11,26 @@ export default class WorldRankComponent extends React.Component {
     console.log('WorldRankComponent', this.props.rankings)
     return (
       <View style={styles.rankContainer}>
-        {this.props.rankings.map(item => console.log('Item', item))}
-        {this.props.rankings.length == 0 &&
+        {this.props.rankings.length <= 0 &&
           <Text style={[styles.textWhite]}>No Rankings</Text>
         }
         {this.props.rankings.length > 0 &&
-          this.props.rankings.map(({item, index}) => {
+          this.props.rankings.map(({country, points}, index) => {
             return (
               <View style={styles.rankRow}>
                 <Text style={styles.rankNumber}>{index+1}</Text>
                 <View style={styles.rankDetail}>
                   <Text style={styles.rankName}>
-                    {item.country}
+                    {country}
                   </Text>
                   <Text style={styles.smallWhiteText}>
-                    {item.points}
+                    {points}
                   </Text>
                 </View>
               </View>
             )
           })
         }
-        <View style={styles.rankRow}>
-          <Text style={styles.rankNumber}>2</Text>
-          <View style={styles.rankDetail}>
-            <Text style={styles.rankName}>
-              Switzerland
-            </Text>
-            <Text style={styles.smallWhiteText}>
-              9,876,653
-            </Text>
-          </View>
-        </View>
-        <View style={styles.rankRow}>
-          <Text style={styles.rankNumber}>3</Text>
-          <View style={styles.rankDetail}>
-            <Text style={ styles.rankName}>
-              Costa Rica
-            </Text>
-            <Text style={styles.smallWhiteText}>
-              9,876,653
-            </Text>
-          </View>
-        </View>
-        <View style={styles.rankRow}>
-          <Text style={styles.rankNumber}>4</Text>
-          <View style={styles.rankDetail}>
-            <Text style={styles.rankName}>
-              Sweden
-            </Text>
-            <Text style={styles.smallWhiteText}>
-              9,876,653
-            </Text>
-          </View>
-        </View>
-        <View style={styles.rankRow}>
-          <Text style={styles.rankNumber}>5</Text>
-          <View style={styles.rankDetail }>
-            <Text style={styles.rankName}>
-              Norway
-            </Text>
-            <Text style={styles.smallWhiteText}>
-              9,876,653
-            </Text>
-          </View>
-        </View>
       </View>
     );
   }
