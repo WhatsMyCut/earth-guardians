7ad3f4333ccfed7ba5d7cd402ffc685e14f98733
Mike Taylor <mike@whatsmycut.com>
2019-03-11 02:20:35 -0700

    Adding EventHit... needs work

--- mobile/services/googleAnalytics.js
+++ mobile/services/googleAnalytics.js
@@ -1,9 +1,9 @@
 import { Analytics, PageHit } from 'expo-analytics';
-export default googleAnalytics = function(page_name) {
+export const analytics = new Analytics('UA-131896215-1');
+export const pageHit = (page_name) => {
   return () => {
-    console.log('googleAnalytics', page_name)
+    console.log('googleAnalytics.pageHit', page_name)
     try {
-      const analytics = new Analytics('UA-131896215-1');
       analytics
         .hit(new PageHit(page_name))
         .then(() => console.log('GA success'))
@@ -12,5 +12,20 @@ export default googleAnalytics = function(page_name) {
       console.log(e);
     }
   };
-
 }
+export const eventHit = (phone, item_id) => {
+  return () => {
+    console.log('googleAnalytics.eventHit', phone, item_id)
+    try {
+      analytics
+        .event(new Event('MyAction', 'Press', phone, item_id))
+        .then(() => {
+          this.setState({in:false})
+        })
+        .catch(e => console.log(e.message));
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
